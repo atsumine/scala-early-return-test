8ce5af1997305f21a6d0fe5a5db3f66a68845b2b
atsu <atsu9615@me.com>
2019-06-05 00:27:03 +0900

    ベンチマーク用記述を追加

--- src/main/scala/benchmark/EarlyReturnTest.scala
+++ src/main/scala/benchmark/EarlyReturnTest.scala
@@ -1,22 +1,29 @@
-import scala.util.control.Breaks
 
-object EaarlyReturnTest {
+package benchmark
+
+import scala.util.control.Breaks
+import org.openjdk.jmh.annotations.{Benchmark, Scope, State}
+
+@State(Scope.Thread)
+class EaarlyReturnTest {
   val numSeq = Seq(1,2,3,4,5)
 
   val b = new Breaks
 
-  def existsWithBreak(num: Int): Boolean = {
+  @Benchmark
+  def existsWithBreak(): Boolean = {
     var result = false
     b.breakable {
       for (x <- numSeq)
-        if (x == num) { result = true; b.break }
+        if (x == 5) { result = true; b.break }
     }
     result
   }
 
-  def existsWithReturn(num: Int): Boolean = {
+  @Benchmark
+  def existsWithReturn(): Boolean = {
       for (x <- numSeq)
-        if (x == num) { return true }
+        if (x == 5) { return true }
     false
   }
 }
